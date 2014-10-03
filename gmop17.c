0dd34b89d76b92c26f58a5e3797ed1b3c0f82e61
ChengBo <bywetech@gmail.com>
2014-10-03 21:48:48 +0800

    Create gmop17.c

---
+++ gmop17.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+  
+#include <sys/time.h>
+#include <math.h>
+
+#include "gmp16.h"
+#include "gmp15.h"
+#include "gmp14.h"
+
+
+
+int main()
+{
+	f_menu();
+	
+	
+	 
+}
